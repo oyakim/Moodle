39473d385b45f36bd0ca91cd0b871f995a35a4a1
oyakim <70718950+oyakim@users.noreply.github.com>
2020-09-14 13:53:07 +0300

    Update setup_webserver.sh

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -75,8 +75,8 @@ check_fileServerType_param $fileServerType
   # install the base stack
   # passing php versions $phpVersion
   sudo apt-get -y install varnish php$phpVersion php$phpVersion-ldap  php$phpVersion-cli php$phpVersion-curl php$phpVersion-zip php-pear php$phpVersion-mbstring php$phpVersion-dev mcrypt
-  sudo apt-get install language-pack-tr
-  sudo apt-get install language-pack-en
+  sudo apt-get -y install language-pack-tr
+  sudo apt-get -y install language-pack-en
   # if webservertype is nginx then apache2 will be masked.
   service=apache2
   if [ "$webServerType" = "nginx" ]; then
